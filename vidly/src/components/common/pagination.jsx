d0b1ae745ebb3dd1ae24e6c21c385cd2ed9ebe3c
taratorn123 <60212231+taratorn123@users.noreply.github.com>
2022-11-21 12:35:02 +0700

    added filtering

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import _ from "lodash";
+import PropTypes from "prop-types";
+
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize);
@@ -25,6 +27,12 @@ const Pagination = (props) => {
       </ul>
     </nav>
   );
+};
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired,
 };
 
 export default Pagination;
---
+++ vidly/src/components/moviesTable.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Like from "./common/like";
+
+const MoviesTable = (props) => {
+  const { movies, onDelete, onLike, onSort } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th onClick={() => onSort("title")} scope="col">
+            Title
+          </th>
+          <th onClick={() => onSort("genre.name")} scope="col">
+            Genre
+          </th>
+          <th onClick={() => onSort("numberInStock")} scope="col">
+            Stock
+          </th>
+          <th onClick={() => onSort("dailyRentalRate")} scope="col">
+            Rate
+          </th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like onLikeToggle={() => onLike(movie)} liked={movie.liked} />
+            </td>
+            <td>
+              <button
+                onClick={() => onDelete(movie)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
